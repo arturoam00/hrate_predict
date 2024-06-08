26648cb8b758302433f8b853594669db5105d120
Arturo Abril Martinez <arturoabrilm@gmail.com>
2024-06-08 17:28:02 +0200

    just downloads data to  folder

---
+++ README.md
@@ -0,0 +1,7 @@
+For the `get_data.sh` script to work, save an environment variable `BIN_ID` in a `.env` file, like
+
+```
+# .env
+BIN_ID=blabei80o1zfv43
+
+```
---
+++ get_data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Get the bin id 
+source .env
+
+# Base URL
+base_url="https://filebin.net/$BIN_ID/"
+
+files=(
+    "Accelerometer.csv"
+    "Barometer.csv"
+    "Gyroscope.csv"
+    "Linear_Accelerometer.csv"
+    "Location.csv"
+    "Magnetometer.csv"
+    "Proximity.csv"
+    # "time.csv" ??
+
+)
+
+# Create the data directory if it doesn't exist
+mkdir -p data/$BIN_ID
+
+# Loop over the names and use wget to download each file to the 1data directory
+for file in "${files[@]}"
+do
+    url="${base_url}${file}"
+    echo "Downloading $url to data/$BIN_ID ..."
+    wget -NP data/$BIN_ID "$url"
+done
+
+echo "All downloads completed."
+
